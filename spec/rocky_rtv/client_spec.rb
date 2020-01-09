417b8195c083f9b30df25058d6b7bd131ee04459
Rohit Parulkar <rohit.parulkar@gmail.com>
2020-01-09 13:11:45 -0500

    fix a test failing with Rspec syntax

--- spec/rocky_rtv/client_spec.rb
+++ spec/rocky_rtv/client_spec.rb
@@ -2,19 +2,15 @@ require 'spec_helper'
 
 RSpec.describe RockyRTV::Client do
 	
+	let(:client) { RockyRTV::Client.new() }
 
 	it "should not raise an error if no API key is supplied" do
 		RockyRTV::Client.new()
 	end
 
-	context "#new client" do
-		before(:each) do 
-			let(:client) { RockyRTV::Client.new() }
-		end
 
-		it "should return the client object" do
-			client.must_be_kind_of(RockyRTV::Client)
-		end
+	it "should return the client object" do
+		expect(client).to be_kind_of(RockyRTV::Client)
 	end
 
 end
